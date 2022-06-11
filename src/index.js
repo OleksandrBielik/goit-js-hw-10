a475946ec90bc8cecae9da44cdd02bd0d512a724
OleksandrBielik <oleksandrbielik.postmail@gmail.com>
2022-06-11 19:33:44 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import './css/styles.css';
+import { fetchCountries } from './js/fetchCountries';
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
+const inputElement = document.getElementById('search-box');
+
+inputElement.addEventListener('input', debounce(() => {
+    fetchCountries(inputElement.value)
+}, DEBOUNCE_DELAY)) 
+
+
+
